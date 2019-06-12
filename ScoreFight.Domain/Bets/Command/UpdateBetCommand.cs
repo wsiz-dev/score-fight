76ec4416787208e231547b71d578db928247ec36
Dawid Pereira <pereiradawid@outlook.com>
2019-06-12 20:54:48 +0200

    Added UpdateBetCommand

---
+++ ScoreFight.Domain/Bets/Command/UpdateBetCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ScoreFight.Domain.Bets.Command
+{
+    public class UpdateBetCommand : ICommand
+    {
+        public string PlayerId { get; }
+
+        public string MatchId { get; }
+
+        public int TeamBet { get; }
+
+        public int PointsBet { get; }
+
+
+        public UpdateBetCommand(string playerId, string matchId, int teamBet, int pointsBet)
+        {
+            if (string.IsNullOrWhiteSpace(playerId)) throw new ArgumentException(nameof(playerId));
+            PlayerId = playerId;
+
+            if (string.IsNullOrWhiteSpace(matchId)) throw new ArgumentException(nameof(matchId));
+            MatchId = matchId;
+
+            if (teamBet < 0) throw new ArgumentException(nameof(teamBet));
+            TeamBet = teamBet;
+
+            if (pointsBet < 0) throw new ArgumentException(nameof(pointsBet));
+            PointsBet = pointsBet;
+        }
+    }
+}
---
+++ ScoreFight.Domain/Bets/Command/UpdateBetCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ScoreFight.Domain.Bets.Validators;
+using ScoreFight.Domain.Matches;
+
+namespace ScoreFight.Domain.Bets.Command
+{
+    public class UpdateBetCommandHandler : ICommandHandler<UpdateBetCommand>
+    {
+        private readonly IBetRepository _betRepository;
+        private readonly IMatchesRepository _matchesRepository;
+        private readonly BetCommandValidator _betCommandValidator;
+
+        public UpdateBetCommandHandler(IBetRepository betRepository, IMatchesRepository matchesRepository, BetCommandValidator betCommandValidator)
+        {
+            _betRepository = betRepository;
+            _matchesRepository = matchesRepository;
+            _betCommandValidator = betCommandValidator;
+        }
+
+        public void Handle(UpdateBetCommand command)
+        {
+            var userId = Guid.Parse(command.PlayerId);
+            var matchId = Guid.Parse(command.MatchId);
+            var match = _matchesRepository.GetById(matchId);
+
+            _betCommandValidator.CheckIfMatchDoesNotExist(match, command.MatchId);
+            _betCommandValidator.CheckIfMatchAlreadyStarted(match);
+
+            var bet = _betRepository.GetBet(userId, matchId);
+
+            if (bet == null)
+            {
+                throw new NullReferenceException($"Bet for PlayerId: '{command.PlayerId}' and MatchId: ' {command.MatchId} ' does not exists.");
+            }
+
+            _betRepository.Save(bet);
+        }
+    }
+}
